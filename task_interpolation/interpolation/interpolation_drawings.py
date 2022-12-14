d603283eb37c1e6775353e1d3265070a671e6c24
Svyatoslav Lipatov <angboroda2@mail.ru>
2022-12-14 16:13:15 +0800

    math
    interpolation

---
+++ task_interpolation/interpolation/interpolation_drawings.py
@@ -0,0 +1,40 @@
+from interpolation.interpolation_calculations import calculation_lagrange_polynomial
+import matplotlib.pyplot as plt
+
+def show_draw(x, y):
+    #Вывод на экран. Визуализация
+    plt.plot(x, y)
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.grid()
+    plt.show()
+
+def draw_linear_interpolation(x, y, xp, yp):
+    #Линейная интерполяция. Визуализация
+    plt.title("Interpolation for point")
+    plt.scatter(x, y, color='blue')
+    plt.scatter(xp, yp, color = "red")
+    show_draw(x, y)
+
+def draw_piecewise_linear_interpolation(x, y, xp, yp):
+    #Кусочно-линейная интерполяция. Визуализация 
+    plt.title("Interpolation for point")
+    plt.scatter(x, y, color='blue')
+    plt.scatter(xp, yp, color = "orange")
+    show_draw(x, y)
+
+def draw_lagrange_polynomial(x, y, xp, yp):
+    #Полином Лагранжа. Визуализация 
+    plt.title("Lagrange interpolation polynomial")
+    plt.scatter(xp, yp, color="orange")
+    plt.axis([min(x) - abs(xp) - 5, max(x) + xp + 5, min(y) - abs(yp * 1.5) - 5, max(y) + abs(yp * 1.5)])
+    x_new =  []
+    min_x = ((min(x) - xp) * 10)
+    max_x = ((max(x) + xp) * 10 + 1)
+    for i in range(int(min_x), int(max_x)):
+        x_new.append(i / 10)
+    y_new = []
+    for item in x_new:
+        a = calculation_lagrange_polynomial(x, y, item)
+        y_new.append(a)
+    show_draw(x_new, y_new)
---
+++ task_interpolation/interpolation/interpolation_output.py
@@ -0,0 +1,8 @@
+
+def show_title(title):
+    #Вывод задания на экран. Вывод
+    print(title)
+
+def show_res(res, msg):
+    #Вывод результата. Вывод
+    print(f"{msg}: {res}")
