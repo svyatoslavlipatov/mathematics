a766f92cbacbdd3dec7113891b1b4ce87d5f9af1
Svyatoslav Lipatov <92099105+svyatoslavlipatov@users.noreply.github.com>
2022-12-15 15:25:32 +0800

    Update interpolation_test.py

--- task_interpolation/interpolation/interpolation_test.py
+++ task_interpolation/interpolation/interpolation_test.py
@@ -8,7 +8,6 @@ def test_linear_interpolation():
     exp = 7.0
     assert res == exp
 
-
 def test_piecewise_linear_interpolation():
     matrix = [[1, 2], [3, 4], [3.5, 3], [6, 7]]
     x, y = inter.get_x_and_y(matrix)
@@ -16,7 +15,6 @@ def test_piecewise_linear_interpolation():
     res = inter.calculation_piecewise_linear_interpolation(x, y, xp)
     exp = [-0.5, 4.0, 3.0, 5.4, 11.8]
     assert res == exp
-
 
 def test_lagrange_polynomial():
     matrix = [[1, 2], [3, 4], [3.5, 3], [6, 7]]
